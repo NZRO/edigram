712360190905d8659fece50cbe4e1322d3622226
Edison Lockwood <risingdowntothetop@hotmail.com>
2020-02-27 17:13:03 -0800

    Initial Commit

---
+++ edigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  edigram
+//
+//  Created by EDISON LOCKWOOD on 2/27/20.
+//  Copyright © 2020 EDISON LOCKWOOD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ edigramTests/edigramTests.swift
@@ -0,0 +1,34 @@
+//
+//  edigramTests.swift
+//  edigramTests
+//
+//  Created by EDISON LOCKWOOD on 2/27/20.
+//  Copyright © 2020 EDISON LOCKWOOD. All rights reserved.
+//
+
+import XCTest
+@testable import edigram
+
+class edigramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
